91420653ad4bfcc37bfc54b0fa684bd038c1f10d
CDelph <delphine@tela-botanica.org>
2022-08-23 18:14:02 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+    </main><!-- .site-main -->
+  </div><!-- .content-area -->
+
+<?php get_footer(); ?>
