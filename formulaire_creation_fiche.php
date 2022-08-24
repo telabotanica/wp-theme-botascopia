1e4405dd1d4b55fa4ab88d643a4c67a92c27574c
CDelph <delphine@tela-botanica.org>
2022-08-24 15:21:43 +0200

    Create formulaire_creation_fiche.php

---
+++ formulaire_creation_fiche.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    Template Name: Formulaire en front création de fiche
+*/
+	acf_form_head();
+
+    $args = array(
+        'post_id' => 'new_post', // On va créer une nouvelle publication
+        'new_post' => array(
+            'post_type' => 'article', // Enregistrer dans l'annuaire
+            'post_status' => 'draft', // Enregistrer en brouillon
+        ),
+        'field_groups' => array( 12 ), // L'ID du post du groupe de champs
+        'submit_value' => 'Enregistrer la fiche', // Intitulé du bouton
+        'updated_message' => "Votre demande a bien été prise en compte.",
+    );
+
+    acf_form( $args ); // Afficher le formulaire
+?>
