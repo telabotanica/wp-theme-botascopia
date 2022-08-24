061674fba1355ab0d891ca6ccf7de3e7f5ef776e
CDelph <delphine@tela-botanica.org>
2022-08-24 11:46:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
+      <nav id="navigation">
+<?php
+wp_nav_menu(
+  array(
+    'theme_location' => 'main-menu',
+    'menu_id' => 'primary-menu',
+ )
+);
+?>
+</nav>
     </main><!-- .site-main -->
   </div><!-- .content-area -->
 
