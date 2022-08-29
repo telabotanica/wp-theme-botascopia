cc303c23b9d3e316dabbbc1219c544251575f695
CDelph <delphine@tela-botanica.org>
2022-08-29 16:36:26 +0200

    Update formulaire_creation_fiche.php

--- formulaire_creation_fiche.php
+++ formulaire_creation_fiche.php
@@ -19,7 +19,7 @@ query_posts(array(
 			<p><?php echo get_the_excerpt(); ?></p>
 		</div>
 <?php endwhile;
-if (is_null(the_field( 'nom_scientifique' ))  == false) {?>	
+if (is_null(the_field( 'nom_scientifique' ))  === false) {?>	
 
 	
 <div >
