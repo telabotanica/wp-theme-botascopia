55717d28c2d23964c222fc6de6c2a720834b484d
kstefanini <killian@stefanini.fr>
2022-09-01 14:09:48 +0200

    Add ACF fields auto export

--- functions.php
+++ functions.php
@@ -5,4 +5,11 @@ function register_my_menu(){
 }
 add_action( 'after_setup_theme', 'register_my_menu' );
 
+// auto export acf fields after each saved change
+function acf_export_json( $path ) {
+  $path = get_stylesheet_directory() . '/acf-json';
+  return $path;
+}
+add_filter( 'acf/settings/save_json', 'acf_export_json' );
+
 ?>
