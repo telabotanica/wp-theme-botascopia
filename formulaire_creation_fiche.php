f0669d8e17642687f80013524d76a591b0b96a41
CDelph <delphine@tela-botanica.org>
2022-10-06 16:13:00 +0200

    test utilisateur debug

--- formulaire_creation_fiche.php
+++ formulaire_creation_fiche.php
@@ -36,7 +36,7 @@ if (isset($_GET['p'])) {
 	<?php endwhile;
 	$auteur_autorise = true;
 	$current_user = strval (get_current_user_id());
-	$post_author = get_post(get_the_ID())->post_author;
+	$post_author = get_the_author();
 		/*false;
 	if ($current_user !== 0) {
 		// si l'auteur du post n'est pas l'admin des fiches
