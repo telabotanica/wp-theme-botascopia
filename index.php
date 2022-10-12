fece54ec94d470ee8979ee33def361d906928ddd
CDelph <delphine@tela-botanica.org>
2022-10-12 17:14:12 +0200

    index test ajout content

--- index.php
+++ index.php
@@ -11,6 +11,17 @@ wp_nav_menu(
 </nav>
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
+          <?php if ( have_posts() ) : ?>
+
+			<?php
+			// Start the loop.
+			while ( have_posts() ) :
+				the_post();
+				?>
+			<?php endwhile; ?>
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
 
     </main><!-- .site-main -->
   </div><!-- .content-area -->
