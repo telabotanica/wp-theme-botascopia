401ba6867acfa1ec25386f3a1e06971667bc60ec
CDelph <delphine@tela-botanica.org>
2022-10-18 17:20:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 	   'showposts' => 10
     ));
 	 while (have_posts()) : the_post(); ?>
-		<div><a href='http://<?php $_SERVER['HTTP_HOST']?>/formulaire/?p=<?php the_title() ?>' target="_blank"><?php the_field( 'nom_scientifique' ); ?></a></div>
+		<div><a href='http://<?php echo $_SERVER['HTTP_HOST'];?>/formulaire/?p=<?php the_title() ?>' target="_blank"><?php the_field( 'nom_scientifique' ); ?></a></div>
 	<?php endwhile;
         echo "<button onclick=\"window.location.href = '".wp_logout_url( $_SERVER['HTTP_HOST'] )."';\">Se déconnecter</button>"; 
     } else {
