5afe22883f4cf570521332bd4fb1ddf8cd142396
arthur-tela <arthur@tela-botanica.org>
2023-02-09 09:07:22 +0100

    affichage section tige

--- fiche_pdf.php
+++ fiche_pdf.php
@@ -83,7 +83,9 @@
                             <div class="picture-ref"><?php echo $index_photos;?></div>
                         <?php endif; ?>
                         <p>La tige aérienne est <?php echo $tige['tige_aerienne'];?>
-                            <?php if ($tige['tige_aerienne'] != 'non visible'):;?>, <?php echo $type_tige;?>, <?php echo $tige['ramification'];?>, à section <?php echo $section_tige;?>.<br>Sa surface est <?php echo $surface_tige;?> au moins quand elle est jeune.</p>
+                            <?php if ($tige['tige_aerienne'] != 'non visible'):;?>, <?php echo $type_tige;?>, <?php echo $tige['ramification'];?>
+                                <?php if ($section_tige != 'pleine'):;?>, à section <?php echo $section_tige;?>
+                                <?php endif; ?>.<br>Sa surface est <?php echo $surface_tige;?> au moins quand elle est jeune.</p>
                             <?php endif; ?>
                     <?php } ?>
                 </div>
