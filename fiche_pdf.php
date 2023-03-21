353a3fb5179316796331d6d9b87260867bff912c
arthur-tela <arthur@tela-botanica.org>
2023-03-21 19:29:16 +0100

    pdf_new_try

--- fiche_pdf.php
+++ fiche_pdf.php
@@ -743,7 +743,7 @@
     var opt = {
     //margin: [15, 0, 15, 0],
     filename: `bouh.pdf`,
-    image: { type: 'png', quality: 1 },
+    image: { type: {'png', 'svg'}, quality: 1 },
     html2canvas: {
     //dpi: 300,
     scale:4,
