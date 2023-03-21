a4b81cd3f1c77dd812f440f3ffd39e8af1517134
arthur-tela <arthur@tela-botanica.org>
2023-03-21 19:35:35 +0100

    pdf_new_try

--- fiche_pdf.php
+++ fiche_pdf.php
@@ -738,7 +738,7 @@
 <?php endif; ?>
 </body>
 </html>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js" integrity="sha512-YcsIPGdhPK4P/uRW6/sruonlYj+Q7UHWeKfTAkBW+g83NKM+jMJFJ4iAPfSnVp7BKD4dKMHmVSvICUbE/V1sSw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.js" integrity="sha512-dVlVERqW+tvhXtyR9eeRRtvoMlTrvtrXpokQOxGqiavf5+LaNjgrZ3lcPw7lsqUETwWlyg91lQhcLFejtyrnjQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
     var opt = {
     //margin: [15, 0, 15, 0],
