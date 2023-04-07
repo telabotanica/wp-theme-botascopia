8ab6d4d0e5fed2ae41bba977b64eb8fa410ad7e7
arthur-tela <arthur@tela-botanica.org>
2023-04-07 13:06:05 +0200

    adding accordion component

---
+++ components/accordion/accordion.php
@@ -0,0 +1,41 @@
+<?php function botascopia_component_accordion($data) {
+
+    $defaults = [
+        'title_level' => get_sub_field('title_level'),
+        'items' => get_sub_field('items'),
+        'modifiers' => []
+    ];
+
+    $data = botascopia_styleguide_data($defaults, $data);
+    $data->modifiers = botascopia_styleguide_modifiers_array(['component', 'component-accordion', 'js-accordion'], $data->modifiers);
+
+    printf(
+        '<div class="%s" data-accordion-prefix-classes="component-accordion">',
+        implode(' ', $data->modifiers)
+    );
+
+    if ( $data->items ):
+
+        foreach ($data->items as $item) :
+
+            echo '<div class="js-accordion__panel component-accordion__panel">';
+
+            $item = (object) $item;
+
+            printf(
+                '<h%s class="js-accordion__header component-accordion__header">%s</h%s>',
+                $data->title_level,
+                $item->title,
+                $data->title_level
+            );
+
+            echo $item->content;
+
+            echo '</div>';
+
+        endforeach;
+
+    endif;
+
+    echo '</div>';
+}
---
+++ components/accordion/script.js
@@ -0,0 +1,5 @@
+require('jquery-accessible-accordion-aria/jquery-accessible-accordion-aria.js');
+
+$(function () {
+    $('.js-accordion').accordion();
+});
