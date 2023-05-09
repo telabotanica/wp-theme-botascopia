fce2de1e422f32a5bd5e6abec91cbfab371b27d2
arthur-tela <arthur@tela-botanica.org>
2023-05-09 10:53:19 +0200

    masking_inner_accordion

--- components/inner_accordion/script.js
+++ components/inner_accordion/script.js
@@ -1,22 +1,22 @@
 require('jquery-accessible-accordion-aria/jquery-accessible-accordion-aria.js');
 import $ from 'jquery';
 $(function () {
-    $('.js-accordion').accordion();
+    $('.js-inner-accordion').accordion();
 
     function toutDeplier(){
         let status = $('#bouton-toutdeplier').attr('accordion-status');
 
         if (status == 0){
-            $('.js-accordion__header').attr('aria-expanded', 'true');
-            $('.js-accordion__panel').attr('aria-hidden', 'false');
+            $('.js-inner-accordion__header').attr('aria-expanded', 'true');
+            $('.js-inner-accordion__panel').attr('aria-hidden', 'false');
             $('#bouton-toutdeplier').attr('accordion-status', '1');
             $('#bouton-toutdeplier span').text('Tout replier');
             $('.formulaire-field-status svg use').attr("xlink:href", "#icon-angle-up");
             // $('.formulaire-field-status svg').toggleClass('icon-angle-down icon-angle-up');
 
         } else {
-            $('.js-accordion__header').attr('aria-expanded', 'false');
-            $('.js-accordion__panel').attr('aria-hidden', 'true');
+            $('.js-inner-accordion__header').attr('aria-expanded', 'false');
+            $('.js-inner-accordion__panel').attr('aria-hidden', 'true');
             $('#bouton-toutdeplier').attr('accordion-status', '0');
             $('#bouton-toutdeplier span').text('Tout déplier');
             $('.formulaire-field-status svg use').attr("xlink:href", "#icon-angle-down");
@@ -28,8 +28,8 @@ $(function () {
     $('#bouton-toutdeplier').on('click', toutDeplier);
 
     // Changement de l'icone lorsque l'on deplie/replie un accordion
-    $('.js-accordion__header').on('click', function() {
-        var accordionId = $(this).closest('.js-accordion').attr('id');
+    $('.js-inner-accordion__header').on('click', function() {
+        var accordionId = $(this).closest('.js-inner-accordion').attr('id');
         changerIcone(accordionId);
     });
 
@@ -38,7 +38,7 @@ $(function () {
 function changerIcone(accordionId){
     var use = $("#" + accordionId + " .formulaire-field-status svg use");
     var svg = $("#" + accordionId + " .formulaire-field-status svg");
-    var header = $("#" + accordionId + " .js-accordion__header");
+    var header = $("#" + accordionId + " .js-inner-accordion__header");
 
     if (header.attr("aria-expanded") === "true"){
         use.attr("xlink:href", "#icon-angle-down");
