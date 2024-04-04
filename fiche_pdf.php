bead1a08b21e7e17bcb7db1a5dbb21398ff4789a
BGE31-Thomas <tpjarles@gmail.com>
2024-04-04 11:21:36 +0200

    ajout Fondateurs

--- fiche_pdf.php
+++ fiche_pdf.php
@@ -855,6 +855,7 @@
                             <div class="footer-logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-botascopia.png" alt="Logo Botascopia"></div>
                         </div>
                         <div class="footer-logos-right">
+                            <div>Fondateurs :</div>
                             <div class="footer-logo"><img class="logo-saclay" src="<?php echo get_template_directory_uri(); ?>/images/logo-saclay.png" alt="Logo Université Paris-Saclay"></div>
                             <div class="footer-logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-tela.png" alt="Logo Tela Botanica"></div>
                         </div>
